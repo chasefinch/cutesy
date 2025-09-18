3c27bf59fd7b7c09bac9fa2a5afff2e3ddcbb55d
Chase Finch <chase@finch.email>
2025-09-18 12:40:28 -0400

    Update PyPI metadata

--- setup.py
+++ setup.py
@@ -30,14 +30,13 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.11",
     entry_points={
         "console_scripts": ["cutesy=cutesy.cli:main"],
     },
