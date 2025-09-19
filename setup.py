9373dfb2157f50d1c65ea08ca7200df5b17a0dde
Chase Finch <chase@finch.email>
2025-09-19 13:58:27 -0400

    Bump version for 1.0b11

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with readme_path.open() as readme_file:
 
 setuptools.setup(
     name="cutesy",
-    version="1.0b10",
+    version="1.0b11",
     author="Chase Finch",
     author_email="chase@finch.email",
     description="A linter & formatter for consistent HTML code, or else.",
