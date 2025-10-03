826a67e968855d6d0df7df72570666f498aee54c
Chase Finch <chase@finch.email>
2025-10-03 15:08:38 -0400

    Bump version for 1.0b13

--- homebrew/cutesy.rb
+++ homebrew/cutesy.rb
@@ -7,7 +7,7 @@ class Cutesy < Formula
 
   desc "A linter & formatter for consistent HTML code, or else."
   homepage "https://github.com/chasefinch/cutesy"
-  url "https://github.com/chasefinch/cutesy/archive/refs/tags/v1.0b12.tar.gz"
+  url "https://github.com/chasefinch/cutesy/archive/refs/tags/v1.0b13.tar.gz"
   sha256 "REPLACE_WITH_ACTUAL_SHA256"  # Get via: shasum -a 256 cutesy-VERSION.tar.gz
   license "Apache-2.0"
   # NOTE: Version is defined in cutesy/__init__.py - update URL and SHA256 manually here
--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 """Settings for Cutesy."""
 
-VERSION = "1.0b12"
+VERSION = "1.0b13"
